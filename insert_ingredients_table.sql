caf3a667304699bbe4b1a4420d4b06fd548d7533
temesghentekeste <mail.temesghen.tekeste@gmail.com>
2024-10-30 14:45:09 +0300

    Insert command

---
+++ insert_ingredients_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO ingredient(name, contains_lactose, vegetarian, vegan, gluten_free) VALUES 
+('Eggs', FALSE, TRUE, FALSE, TRUE),
+('Bread', True, TRUE, FALSE, TRUE),
+('Beef Patty', FALSE, TRUE, True, TRUE),
+('Lettuce', FALSE, FALSE, FALSE, TRUE),
+('Yogurt', TRUE, TRUE, TRUE, TRUE);
